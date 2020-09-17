726ef6eecea5c7f4a68802727d89ec6e32a4ec41
Christopher Waters <czw@princeton.edu>
2020-09-17 16:24:24 -0500

    Use list for curve storage.

--- python/lsst/ip/isr/qeCurve.py
+++ python/lsst/ip/isr/qeCurve.py
@@ -35,39 +35,68 @@ from lsst.ip.isr import IsrCalib
 __all__ = ["Curve"]
 
 
-class Curve(IsrCalib):
-    """An arbitrary curve with interpolation.
+class QeCurve(IsrCalib):
+    """Parameter set for quantum efficiency data.
 
     Parameters
     ----------
+    curveType : `str`, optional
+    level : `str`, optional
+    listDict : `dict` [`str` `dict`]
+
+    camera : `lsst.afw.cameraGeom.Camera`, optional
+    detector : `lsst.afw.cameraGeom.Detector`, optional
+        Detector object.  Passed to self.fromDetector() on init.
     log : `lsst.log.Log`, optional
-        Log to write messages to.
-    **kwargs :
-        Parameters to pass to parent constructor.
+        Logger to handle messages.
+    kwargs : `dict`, optional
+        Other keyword arguments to pass to the parent init.
+
+    Raises
+    ------
+    RuntimeError :
+        Raised if
+
+    Notes
+    -----
+    The curve attributes stored are:
+
+    curveType : `str`
+        Type of curve stored.
+    level : `str`
+        Level at which the curve is defined.  Should be one of 'AMP',
+        'DETECTOR', 'FP'.
+    radiusScale : `float`
+        Radius scale factor to apply.  Should be equal to the pixel size.
+    units : `str`
+        Units the wavelengths are tabulated in.
+    curves : `dict` [`str`, `dict`]
+        Dictionary of curve data, indexed by the name of the level
+        segments.  Each contains the following keys:
+
+        dimension : `int`
+            Dimension of throughputs array.
+        radii : `numpy.array`
+            Array of radii samples.
+        wavelengths : `numpy.array`
+            Array of wavelength samples.
+        throughputs : `numpy.array`
+            Array of throughputs.
     """
 
     _OBSTYPE = 'qe_curve'
     _SCHEMA = 'Gen3 qe curves'
     _VERSION = 1.0
 
-    def __init__(self, curveType=None, level=None, filterName=None, listDict=None,
-                 **kwargs):
+    def __init__(self, curveType=None, level=None, radiusScale=1.0, units='nm', **kwargs):
         self.curveType = curveType if curveType else 'QE'
         self.level = level if level else None
-        self.filter = filterName if filterName else None
+        self.radiusScale = radiusScale
+        self.units = units
         self.curves = {}
 
         super().__init__(**kwargs)
-        self.requiredAttributes.update(['curveType', 'level', 'filter'
-                                        'curves'])
-
-        if listDict:
-            for key in listDict:
-                wavelengthUnit = listDict[key].get('unit', "nm")
-                wavelengths = listDict[key].get('wavelengths')
-                throughputs = listDict[key].get('throughputs')
-                self.curve[key] = self.fromLists(wavelengths, throughputs,
-                                                 wavelengthUnit=wavelengthUnit)
+        self.requiredAttributes.update(['curveType', 'level', 'radiusScale', 'units', 'curves'])
 
     def updateMetadata(self, setDate=False, **kwargs):
         """Update calibration metadata.
@@ -85,48 +114,72 @@ class Curve(IsrCalib):
         """
         kwargs['TYPE'] = self.curveType
         kwargs['LEVEL'] = self.level
-        kwargs['FILTER'] = self.filter
+        kwargs['RAD_SCL'] = self.radiusScale
+        kwargs['UNITS'] = self.units
 
         super().updateMetadata(setDate=setDate, **kwargs)
 
-    @staticmethod
-    def standardCurveFromLists(wavelengths, throughputs,
-                               wavelengthUnit="nm"):
-        """Method to generate a curve from wavelength/throughput list
+    def addCurveData(key, dimension, radii=None, wavelengths=None, throughputs=None, length=(0, )):
+        """Add curve data to the calibration dictionary.
+
+        Parameters
+        ----------
+        key : `str`
+            Index for the curve.
+        dimension : `int`
+            Dimension of the throughputs array.
+        radii : `numpy.array`, optional
+            Array of radii samples.
+        wavelengths : `numpy.array`, optional
+            Array of wavelength samples.
+        throughputs : `numpy.array`, optional
+            Array of throughputs.
+        length : `tuple` [`int`], optional
+            Array lengths to initialize to zero if no arrays are
+            supplied.
+
+        Raises
+        ------
+        RuntimeError :
+            Raised if array lengths do not match.
+        KeyError :
+            Raised if the curve to create already exists.
         """
-        if len(wavelengths) != len(throughputs):
-            raise RuntimeError("Wavelength and throughput lists are different length!")
+        if key in self.curves:
+            raise KeyError(f"Key {key} already exists.")
 
-        if wavelengthUnit == 'nm':
-            wavelengths *= 10.0
-        atMin = throughputs[0]
-        atMax = throughputs[-1]
-        return afwImage.TransmissionCurve.makeSpatiallyConstant(throughput=throughputs,
-                                                                wavelengths=wavelengths,
-                                                                throughputAtMin=atMin,
-                                                                throughputAtMax=atMax)
+        if dimension == 1:
+            if radii:
+                raise RuntimeError("Radii suppled for 1d curve.")
 
-    @staticmethod
-    def radialCurveFromLists(radii, wavelengths, throughputs,
-                             radiusScale=1.0, wavelengthUnit="nm"):
-        """Method to generate a curve from a set of throughputs defined on a
-        grid of wavelength/radius.
+            if wavelengths and througputs:
+                if len(wavelengths) != len(throughputs):
+                    raise RuntimeError("Wavelength and throughput arrays differ in length.")
+            elif len(length) == 1:
+                radii = []
+                wavelengths = np.zeros(shape=length)
+                throughputs = np.zeros(shape=length)
+            else:
+                raise RuntimeError("No data supplied.")
+        elif dimension == 2:
+            if radii and wavelengths and throughputs:
+                if througputs.shape != (len(wavelengths), len(radii)):
+                    raise RuntimeError("Throughputs not equal to radii and wavelengths.")
+            elif len(length) == 2:
+                radii = np.zeros(length[1])
+                wavelengths = np.zeros(length[0])
+                throughputs = np.zeros(length)
+            else:
+                raise RuntimeError("No data supplied.")
+        else:
+            raise RuntimeError(f"Unsupported dimension {dimension}.")
 
-        """
-        if len(radii) != throughputs.shape[0]:
-            raise RuntimeError(f"Radii dimension mismatch: {len(radii)} != {throughputs.shape[0]}")
-        if len(wavelengths) != throughputs.shape[1]:
-            raise RuntimeError(f"Wavelength dimension mismatch: {len(wavelengths)} != {throughputs.shape}")
-
-        radii *= radiusScale
-        if wavelengthUnit == 'nm':
-            wavelengths *= 10.0
-
-        return afwImage.TransmissionCurve.makeRadial(throughput=throughputs,
-                                                     wavelengths=wavelengths,
-                                                     radii=radii,
-                                                     throughputAtMin=0.0,
-                                                     throughputAtMax=0.0)
+        # Inputs should be validated now.
+        self.curve[key] = {'dimension': dimension,
+                           'radii' : radii,
+                           'wavelengths' : wavelengths,
+                           'throughputs' : througputs,
+                           }
 
     @classmethod
     def fromDict(cls, dictionary):
@@ -151,34 +204,42 @@ class Curve(IsrCalib):
         calib = cls()
 
         metadata = dictionary['metadata']
-        if calib._OBSTYPE != metadata['OBSTYPE']:
-            raise RuntimeError(f"Incorrect curve supplied.  Expected {calib._OBSTYPE}, "
-                               f"found {metadata['OBSTYPE']}")
-
         calib.setMetadata(metadata)
+
+        calib.calibInfoFromDict(dictionary)
+
         calib.curveType = metadata.get('TYPE', 'QE')
         calib.level = metadata.get('LEVEL', metadata.get('MODE', 'DETECTOR'))
-        calib.filter = metadata.get('FILTER', None)
-
-        calib._detectorName = metadata.get('DETECTOR', None)
-        calib._detectorSerial = metadata.get('DETECTOR_SERIAL', None)
+        calib.radiusScale = metadata.get('RAD_SCL', 1.0)
+        calib.units = metadata.get('UNITS', 'nm')
 
         for key in dictionary['curves']:
-            curveDict = dictionary['curves']['key']
-            if curveDict['dimension'] == 1:
-                calib.curve[key] = cls.standardCurvefromLists(curveDict['wavelengths'],
-                                                              curveDict['throughputs'],
-                                                              wavelengthUnit=curveDict.get('unit', 'nm'))
-            elif curveDict['dimension'] == 2:
-                calib.curve[key] = cls.radialCurveFromLists(curveDict['radii'],
-                                                            curveDict['wavelengths'],
-                                                            curveDict['throughputs'],
-                                                            curveDict.get('radiusScale', 1.0),
-                                                            curveDict.get('unit', 'nm'))
-            else:
-                raise RuntimeError(f"Unknown  dimension: {curveDict['dimension']}")
+            calib.curve[key] = dictionary['curves'][key]
 
         return calib
+
+    def toDict(self):
+        """Return a dictionary containing the calibration properties.
+
+        Returns
+        ----------
+        outDict : `dict`
+            Dictionary of properties.
+        """
+        self.updateMetadata()
+
+        outDict = {}
+        outDict['metadata'] = self.getMetadata()
+        outDict['type'] = self.curveType
+        outDict['level'] = self.level
+        outDict['radiusScale'] = self.radiusScale
+        outDict['units'] = self.units
+
+        outDict.curve = {}
+        for key in self.curves:
+            outDict.curve[key] = self.curves[key]
+
+        return outDict
 
     @classmethod
     def fromTable(cls, tableList):
@@ -208,8 +269,7 @@ class Curve(IsrCalib):
 
         for row in qeTable:
             key = row['key']
-            for column in ['dimension', 'wavelengths' 'throughputs',
-                           'unit', 'radii', 'radiusScale']:
+            for column in ['dimension', 'radii', 'wavelengths' 'throughputs']:
                 if column in row:
                     inDict['curves'][key][column] = row[column]
 
@@ -232,70 +292,144 @@ class Curve(IsrCalib):
         (keys, dimension, wavelengths, throughputs,
          unit, radii, radiusScale) = ([], [], [], [],
                                       [], [], [])
-        for key in self.curves:
-            keys.append(key)
-            dimension.append(self.curves[key]['dimension'])
-            wavelengths.append(self.curves[key]['wavelengths'])
-            throughputs.append(self.curves[key]['throughputs'])
-            unit.append(self.curves[key].get('unit', 'nm'))
-            radii.append(self.curves[key].get('radii', []))
-            radiusScale.append(self.curves[key].get('radiusScale', 0.0))
+        keys = [key for key in self.curves]
+        dimensions = [self.curve[key]['dimension'] for key in self.curves]
+        radii = [self.curve[key]['radii'] for key in self.curves]
+        wavelengths = [self.curve[key]['wavelengths'] for key in self.curves]
+        throughputs = [self.curve[key]['throughputs'] for key in self.curves]
 
         catalog = Table([{'key': keys,
+                          'radii': radii,
                           'dimension': dimension,
                           'wavelengths': wavelengths,
-                          'throughputs': throughputs,
-                          'unit': unit,
-                          'radii': radii,
-                          'radiusScale': radiusScale}])
+                          'throughputs': throughputs}])
         catalog.meta = self.getMetadata().toDict()
 
         return([catalog])
 
-    # Actual science methods
-    def evaluate(self, wavelengths, detector=None, position=None,
-                 kind='linear', bounds_error=False, fill_value=0):
-        """Interpolate the curve at the specified position and wavelength.
+    # Science methods
+    @staticmethod
+    def standardCurveFromLists(wavelengths, throughputs, wavelengthUnit="nm", atMin=None, atMax=None):
+        """Generate a curve from wavelength/throughput list.
 
         Parameters
         ----------
-        wavelength : `astropy.units.Quantity`
-            The wavelength(s) at which to make the interpolation.
-        position : `lsst.geom.Point2D`, optional
-            The position on the detector at which to evaluate the curve.
-        detector : `lsst.afw.cameraGeom.Detector`, optional
-            Is used to find the appropriate curve given the position for
-            curves that vary over the detector.  Ignored in the case where
-            there is only a single curve per detector.
-        kind : `str`, optional
-            The type of interpolation to do (default is 'linear').
-            See documentation for `scipy.interpolate.interp1d` for
-            accepted values.
-        bounds_error : `bool`, optional
-            Raise error if interpolating outside the range of x?
-            (default is False)
-        fill_value : `float`, optional
-            Fill values outside the range of x with this value
-            (default is 0).
+        wavelengths : `numpy.array`
+            Wavelengths the curve is sampled at.
+        throughputs : `numpy.array`
+            Throughput at each wavelength sample.
+        wavelengthUnit : `str`, optional
+            Unit the wavelengths are tabulated in.  Defaults to 'nm'.
+        atMin : `float`, optional
+            Value to use at and below the minimum wavelength value.
+        atMax : `float`, optional
+            Value to use at and above the maximum wavelength value.
+
         Returns
         -------
-        value : `astropy.units.Quantity`
-            Interpolated value(s).  Number of values returned will match the
-            length of `wavelength`.
+        curve : `lsst.afw.image.TransmissionCurve`
+            The transmission curve constructed.
 
         Raises
         ------
-        ValueError
-            If the ``bounds_error`` is changed from the default, it will raise
-            a `ValueError` if evaluating outside the bounds of the curve.
+        RuntimeError :
+            Raised if the wavelength and througput arrays have
+            different lengths.
         """
-        if self.mode == 'AMP':
-            targetAmp = cgUtils.findAmp(detector, Point2I(position))
-            return self.curves[targetAmp].sampleAt(position, wavelengths)
-        elif self.mode in ('DETECTOR', 'IMAGE'):
-            # Should IMAGE have subregions defined?
-            keys = list(self.curves.keys())
-            if len(keys) == 1:
-                return self.curves[keys[0]].sampleAt(position, wavelengths)
-            else:
-                raise RuntimeError("unexpected multi-key value!")
+        if len(wavelengths) != len(throughputs):
+            raise RuntimeError("Wavelength and throughput lists are different length!")
+
+        if wavelengthUnit == 'nm':
+            wavelengthFactor = 10.0
+
+        atMin = atMin if atMin else throughputs[0]
+        atMax = atMax if atMax else throughputs[-1]
+        return afwImage.TransmissionCurve.makeSpatiallyConstant(throughput=throughputs,
+                                                                wavelengths=wavelengths * wavelengthFactor,,
+                                                                throughputAtMin=atMin,
+                                                                throughputAtMax=atMax)
+
+    @staticmethod
+    def radialCurveFromLists(radii, wavelengths, throughputs, radiusScale=1.0, wavelengthUnit="nm",
+                             atMin=None, atMax=None):
+        """Generate a curve from a list of wavelengths and radius points, and
+        a grid of throughput values.
+
+        Parameters
+        ----------
+        radii : `numpy.array`
+            Radii the throughputs are sampled at.
+        wavelengths : `numpy.array`
+            Wavelengths the curve is sampled at.
+        throughputs : `numpy.array`
+            Throughput at each wavelength sample.
+        wavelengthUnit : `str`, optional
+            Unit the wavelengths are tabulated in.  Defaults to 'nm'.
+        atMin : `float`, optional
+            Value to use at and below the minimum wavelength value.
+        atMax : `float`, optional
+            Value to use at and above the maximum wavelength value.
+
+        Returns
+        -------
+        curve : `lsst.afw.image.TransmissionCurve`
+            The transmission curve constructed.
+
+        Raises
+        ------
+        RuntimeError :
+            Raised if the radii, wavelength, and througput arrays have
+            different lengths.
+        """
+        if len(radii) != throughputs.shape[0]:
+            raise RuntimeError(f"Radii dimension mismatch: {len(radii)} != {throughputs.shape[0]}")
+        if len(wavelengths) != throughputs.shape[1]:
+            raise RuntimeError(f"Wavelength dimension mismatch: {len(wavelengths)} != {throughputs.shape}")
+
+        if wavelengthUnit == 'nm':
+            wavelengthFactor = 10.0
+
+        atMin = atMin if atMin else throughputs[0]
+        atMax = atMax if atMax else throughputs[-1]
+        return afwImage.TransmissionCurve.makeRadial(throughput=throughputs,
+                                                     wavelengths=wavelengths * wavelengthFactor,
+                                                     radii=radii / radiusScale,
+                                                     throughputAtMin=0.0,
+                                                     throughputAtMax=0.0)
+
+    def evaluate(self, key):
+        """Generate the afwImage.TransmissionCurve from the calibration
+        dictionary.
+
+        Parameters
+        ----------
+        key : `str`
+            Name of the curve to construct.
+
+        Returns
+        -------
+        curve : `lsst.afw.image.TransmissionCurve`
+            The constructed curve.
+
+        Raises
+        ------
+        KeyError
+            Raised if the requested key does not exist.
+        """
+        if key not in self.curves:
+            raise KeyError(f"No entry {key} found in {self}")
+
+        curveData = self.curves[key]
+        if curveData['dimension'] == 1:
+            return self.standardCurveFromLists(curveData['wavelengths'], curveData['throughputs'],
+                                               wavelengthUnit=self.units,
+                                               atMin=curveData['atMin'],
+                                               atMax=curveData['atMax'])
+        elif curveData['dimension'] == 2:
+            return self.radialCurveFromLists(curveData['radii'],
+                                             curveData['wavelengths'], curveData['throughputs'],
+                                             wavelengthUnit=self.units,
+                                             atMin=curveData['atMin'],
+                                             atMax=curveData['atMax'])
+        else:
+            raise RuntimeError(f"Unsupported curve dimension {curveData['dimension']}")
